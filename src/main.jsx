bec46de49f2bb3e5141d5d603d78284e2abd99c9
Aditya Difa Amanullah <adityadifa123@gmail.com>
2025-10-20 10:31:59 +0700

    kembalikan lagi ke finsihed

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from "react";
-import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import "./assets/global.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <StrictMode>
     <App />
-  </BrowserRouter>
+  </StrictMode>
 );
