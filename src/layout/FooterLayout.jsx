3b414338c2e874480e028b5996d0948fd3bd866b
Aditya Difa Amanullah <adityadifa123@gmail.com>
2025-10-20 10:17:08 +0700

    finished

--- src/layout/FooterLayout.jsx
+++ src/layout/FooterLayout.jsx
@@ -14,7 +14,80 @@ const FooterLayout = () => {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  const MobileView = () => {};
+  const MobileView = () => {
+    return (
+      <>
+        <div className="flex px-3 flex-col">
+          <img src={logoIcon} alt="" className="w-[170px]" />
+          <h1 className="text-[12px] font-semibold">
+            Gali Potensi Anda Melalui Pembelajaran Video di hariesok.id!
+          </h1>
+          <p className="text-xs my-1">
+            Jl. Usman Effendi No. 50 Lowokwaru, Malang
+          </p>
+          <p className="text-xs">+62-877-7123-1234</p>
+          <details>
+            <summary className="font-semibold">Kategori</summary>
+            <div className="flex flex-col gap-1">
+              <a href="" className="border-b-1">
+                Digital & Teknologi
+              </a>
+              <a href="" className="border-b-1">
+                Pemasaran
+              </a>
+              <a href="" className="border-b-1">
+                Manajemen Bisnis
+              </a>
+              <a href="" className="border-b-1">
+                Pengembangan Diri
+              </a>
+              <a href="" className="border-b-1">
+                Desain
+              </a>
+            </div>
+          </details>
+          <details>
+            <summary className="font-semibold">Perusahaan</summary>
+            <div className="flex flex-col gap-1">
+              <a href="" className="border-b-1">
+                Tentang Kami
+              </a>
+              <a href="" className="border-b-1">
+                FAQ
+              </a>
+              <a href="" className="border-b-1">
+                Kebijakan Privasi
+              </a>
+              <a href="" className="border-b-1">
+                Ketentuan Layanan
+              </a>
+              <a href="" className="border-b-1">
+                Bantuan
+              </a>
+            </div>
+          </details>
+          <details>
+            <summary className="font-semibold">Komunitas</summary>
+            <div className="flex flex-col gap-1">
+              <a href="" className="border-b-1">
+                Tips Sukses
+              </a>
+              <a href="" className="border-b-1">
+                Blog
+              </a>
+            </div>
+          </details>
+          <div className="w-[35px] flex flex-row gap-2 my-2">
+            <img src={linkedinIcon} alt="" />
+            <img src={facebookIcon} alt="" />
+            <img src={instagramIcon} alt="" />
+            <img src={twitterIcon} alt="" />
+          </div>
+          <p className="text-sm">@2023 Gerobak Sayur All Rights Reserved.</p>
+        </div>
+      </>
+    );
+  };
   const DeskopView = () => {
     return (
       <>
