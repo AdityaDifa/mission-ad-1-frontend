1602719e3ae462778234a5a291a196c6c6ba50ba
Aditya Difa Amanullah <adityadifa123@gmail.com>
2025-10-20 10:34:13 +0700

    last

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Beranda from "./pages/Beranda";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import NotFound from "./pages/NotFound";
-import { useState } from "react";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Beranda />} />
-        <Route path="/Register" element={<Register />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Beranda />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 };
+
 export default App;
